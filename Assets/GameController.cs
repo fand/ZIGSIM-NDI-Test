1f893afaa46bbc4528a30dfbf353cf9fd197ffca
fand <fand.gmork@gmail.com>
2020-02-05 22:03:46 +0900

    Fix gamma

---
+++ Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Klak.Ndi;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] Text _info;
+    [SerializeField] NdiReceiver _receiver;
+
+    void Update()
+    {
+        _info.text = string.Format(
+            "Source: {0}\nResolution: {1} x {2}",
+            _receiver.sourceName,
+            _receiver.width,
+            _receiver.height
+        );
+    }
+}
